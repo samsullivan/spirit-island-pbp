fc0f33186a3fcdf89bf9519101f324e4eb64e98c
Sam Sullivan <samsullivan09@gmail.com>
2024-09-06 15:12:03 -0700

    implement CockroachDB; other fixes to support Vercel release

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PATH=$PATH:/home/ubuntu/.local/bin
 poetry install --no-root
 poetry run python3 ./manage.py collectstatic --noinput
 poetry run python3 ./manage.py migrate
-exec poetry run gunicorn island.wsgi
+exec poetry run gunicorn api.wsgi
